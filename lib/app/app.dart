827273cee389fc43a799e679510d63eac58e86ae
kiwi520 <roczhmg@163.com>
2021-10-23 11:30:06 +0800

    改为 multiProvider 、consumer2方式处理

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -23,84 +23,85 @@ class _AppState extends State<App> {
         ChangeNotifierProvider<TabBarProvider>(create: (_) => TabBarProvider()),
       ],
       builder: (context, _) {
-        final themeProvider = Provider.of<ThemeProvider>(context);
-        final tabBarProvider =
-            Provider.of<TabBarProvider>(context, listen: true);
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: title,
-          themeMode: themeProvider.themeMode,
-          theme: AppTheme.light,
-          darkTheme: AppTheme.dark,
-          home: DefaultTabController(
-            length: 7,
-            child: Scaffold(
-              appBar: AppBar(
-                // backgroundColor: Colors.orange,
-                title: Text('Welcome',
-                  style: TextStyle(
-                    color: Color(0xFF2D2D2F),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500
-                  ),
-                ),
-                backgroundColor: Colors.white,
-                leading: IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  color: Color(0xFF2D2D2F),
-                  onPressed: () {},
-                ),
-                actions: [ChangeThemeButtonWidget()],
-                bottom: TabBar(
-                  isScrollable: true,
-                  padding: EdgeInsets.only(left: 20),
-                  tabs: [
-                    Tab(
-                      child: Text(
-                          'Latest',
-                          style: TextStyle(
-                            color:Color(0XFF2967FF),
-                          )
+        return Consumer2<ThemeProvider,TabBarProvider>(
+          builder: (context,theme,tabBer, chiild) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: title,
+              themeMode: theme.themeMode,
+              theme: AppTheme.light,
+              darkTheme: AppTheme.dark,
+              home: DefaultTabController(
+                length: 7,
+                child: Scaffold(
+                  appBar: AppBar(
+                    // backgroundColor: Colors.orange,
+                    title: Text('Welcome',
+                      style: TextStyle(
+                          color: Color(0xFF2D2D2F),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500
                       ),
                     ),
-                    Tab(
-                      text: 'World',
+                    backgroundColor: Colors.white,
+                    leading: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      color: Color(0xFF2D2D2F),
+                      onPressed: () {},
                     ),
-                    Tab(
-                      text: 'Business',
+                    actions: [ChangeThemeButtonWidget()],
+                    bottom: TabBar(
+                      isScrollable: true,
+                      padding: EdgeInsets.only(left: 20),
+                      tabs: [
+                        Tab(
+                          child: Text(
+                              'Latest',
+                              style: TextStyle(
+                                color:Color(0XFF2967FF),
+                              )
+                          ),
+                        ),
+                        Tab(
+                          text: 'World',
+                        ),
+                        Tab(
+                          text: 'Business',
+                        ),
+                        Tab(
+                          text: 'Fourth',
+                        ),
+                        Tab(
+                          text: 'Lifestyle',
+                        ),
+                        Tab(
+                          text: 'Technology',
+                        ),
+                        Tab(
+                          text: 'Sports',
+                        )
+                      ],
                     ),
-                    Tab(
-                      text: 'Fourth',
-                    ),
-                    Tab(
-                      text: 'Lifestyle',
-                    ),
-                    Tab(
-                      text: 'Technology',
-                    ),
-                    Tab(
-                      text: 'Sports',
-                    )
-                  ],
+                  ),
+                  body: tabBer.currentScreen,
+                  bottomNavigationBar: BottomNavigationBar(
+                    currentIndex: tabBer.currentTab,
+                    onTap: (int index) {
+                      tabBer.currentIndex = index;
+                    },
+                    items: [
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.explicit_outlined), label: '发现'),
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.add_a_photo_outlined), label: '添加'),
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.account_circle_outlined), label: '添加'),
+                    ],
+                  ),
                 ),
               ),
-              body: tabBarProvider.currentScreen,
-              bottomNavigationBar: BottomNavigationBar(
-                currentIndex: tabBarProvider.currentTab,
-                onTap: (int index) {
-                  tabBarProvider.currentIndex = index;
-                },
-                items: [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.explicit_outlined), label: '发现'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.add_a_photo_outlined), label: '添加'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.account_circle_outlined), label: '添加'),
-                ],
-              ),
-            ),
-          ),
+            );
+          },
         );
       },
     );
